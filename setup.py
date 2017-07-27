a5f0101d9bf6e1a3718be789ce4292ddf85d0b6a
Son A. Pham <sonph@users.noreply.github.com>
2017-07-27 16:38:16 -0700

    Add setup.py and setup.cfg prepare for pypi #19

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+    name = 'gdaxcli',
+    packages = ['gdaxcli'],
+    version = '0.1',
+    description = 'Commandline client for trading on GDAX',
+    author = 'Son Pham',
+    author_email = 'sp@sonpham.me',
+    url = 'https://github.com/sonph/gdaxcli',
+    download_url = 'https://github.com/sonph/gdaxcli/archive/0.1.0.zip',
+    keywords = ['gdax', 'cli', 'tool', 'client', 'cryptocurrency', 'bitcoin',
+      'ethereum', 'trading'],
+    classifiers = [],
+)
+
+# To upload to PyPI:
+# python setup.py register -r pypi
+# python setup.py sdist upload -r pypi
